53993366fb6e1ee583e064310402f92448f027be
LunaFr0st <codyamies@gmail.com>
2017-10-31 17:06:14 +1100

    Uploaded projected

---
+++ Assets/Scripts/AI/AIAgent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+namespace TowerDefense
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class AIAgent : MonoBehaviour
+    {
+       
+        public Transform target;
+
+        private NavMeshAgent nav;
+        // Use this for initialization
+        void Awake()
+        {
+            nav = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (target != null)
+            {
+                nav.SetDestination(target.position);
+            }
+        }
+    }
+}
