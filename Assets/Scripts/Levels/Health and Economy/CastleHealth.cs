e5b844229cdfc1f32d3c504184fb79e964c152c0
LunaFr0st <codyamies@gmail.com>
2017-11-17 09:44:54 +1100

    Added CastleHealth Script Added PlayerMoney Script

---
+++ Assets/Scripts/Levels/Health and Economy/CastleHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class CastleHealth : MonoBehaviour
+    {
+        protected float health = 1000f;
+        protected bool castleDestroyed = false;
+        void Update()
+        {
+            if (health <= 0)
+            {
+                health = 0;
+                castleDestroyed = true;
+            }
+            if (castleDestroyed)
+            {
+                health = 0;
+            }
+        }
+        protected void recieveDamage(int damageReceived)
+        {
+            if(!(health <= 0))
+            {
+                health = health - damageReceived;
+            }
+        }
+        protected void healCastle(int healAmount)
+        {
+            if(!(health <= 0))
+            health = health + healAmount;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Levels/Health and Economy/PlayersMoney.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class PlayersMoney : LevelGUIManager
+    {
+        protected int gold;
+        public string goldText;
+
+        public void Update()
+        {
+            if (gold <= 0)
+            {
+                gold = 0;
+            }
+            if (gold > 1000000)
+            {
+                goldText = "1000000+";
+            }
+            else
+            {
+                goldText = gold.ToString();
+            }
+        }
+        protected void SpendGold(int amountSpent)
+        {
+            if(!(gold < amountSpent))
+            {
+                gold = gold - amountSpent;
+            }
+            
+        }
+        protected void RecieveGold(int gainedGold)
+        {
+            gold = gold + gainedGold;
+        }
+    }
+}
+
